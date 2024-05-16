e43b24f70197aff34829b4c1d41a6406f58aea62
Dagmar Dinjens <john.doe1@mail.com>
2024-05-16 17:02:46 +0200

    order met adress optionaliteit

---
+++ luxuryproducts-backend/src/main/java/com/example/todoappdeel3/controller/AdressController.java
@@ -0,0 +1,27 @@
+package com.example.todoappdeel3.controller;
+
+import com.example.todoappdeel3.dao.AdressDAO;
+import com.example.todoappdeel3.dto.AdressDTO;
+import com.example.todoappdeel3.models.Adress;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/adress")
+public class AdressController {
+    private final AdressDAO adressDAO;
+
+    public AdressController(AdressDAO adressDAO) {
+        this.adressDAO = adressDAO;
+    }
+
+
+    @PostMapping
+    public ResponseEntity<String> createAdress(@RequestBody AdressDTO adressDTO){
+        this.adressDAO.createAdress(adressDTO);
+        return ResponseEntity.ok("Created a product");
+    }
+}
--- luxuryproducts-backend/src/main/java/com/example/todoappdeel3/services/UserService.java
+++ luxuryproducts-backend/src/main/java/com/example/todoappdeel3/services/UserService.java
@@ -1,7 +1,10 @@
 package com.example.todoappdeel3.services;
 
+
+import com.example.todoappdeel3.config.JWTUtil;
 import com.example.todoappdeel3.dao.UserRepository;
 import com.example.todoappdeel3.models.CustomUser;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,19 +16,32 @@ import java.util.Collections;
 @Service
 public class UserService implements UserDetailsService {
 
-    private final UserRepository userDAO;
+    private final UserRepository userRepository;
+    private final HttpServletRequest request;
+    private final JWTUtil jwtUtil;
 
-    public UserService(UserRepository userDAO) {
-        this.userDAO = userDAO;
+    public UserService(UserRepository userRepository, HttpServletRequest request, JWTUtil jwtUtil) {
+        this.userRepository = userRepository;
+        this.request = request;
+        this.jwtUtil = jwtUtil;
     }
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        CustomUser customUser = userDAO.findByEmail(email);
+        CustomUser customUser = userRepository.findByEmail(email);
 
         return new User(
                 email,
                 customUser.getPassword(),
                 Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER")));
+    }
+
+    public CustomUser getUserByJWT(){
+        String authHeader = request.getHeader("Authorization");
+        String jwt = authHeader.substring(7);
+        String userEmail = this.jwtUtil.validateTokenAndRetrieveSubject(jwt);
+
+        CustomUser user = userRepository.findByEmail(userEmail);
+        return user;
     }
 }
