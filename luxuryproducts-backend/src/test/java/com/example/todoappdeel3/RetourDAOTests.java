fbfe074e56c7f64db2843d108a44e289c3e95718
Kevin van Dijk <john.doe3@mail.com>
2024-06-19 14:43:53 +0200

    Test retourverzoek beoordelen

--- luxuryproducts-backend/src/test/java/com/example/todoappdeel3/RetourDAOTests.java
+++ luxuryproducts-backend/src/test/java/com/example/todoappdeel3/RetourDAOTests.java
@@ -14,7 +14,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import com.example.todoappdeel3.utils.StaticDetails;
 import org.junit.jupiter.api.Test;
 
-import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
@@ -31,12 +30,6 @@ public class RetourDAOTests {
     private RetourRequestRepository retourRequestRepository;
     @Mock
     private OrderItemRepository orderItemRepository;
-    @Mock
-    private UserRepository userRepository;
-    @Mock
-    private OrderRepository orderRepository;
-    @Mock
-    private RetourReasonRepository retourReasonRepository;
 
     @InjectMocks
     private RetourDAO retourDAO;
@@ -45,53 +38,63 @@ public class RetourDAOTests {
 
     private RetourRequestDTO dummyRetourRequestDTO;
     private RetourRequest dummyRetourRequest;
-    private Set<OrderItem> dummyRetouredProducts;
     private OrderItem dummyRetouredProduct;
+    private Set<OrderItem> dummyRetouredProducts;
     private ProductSpecificationType dummyProduct;
-    private PlacedOrder dummyPlacedOrder;
-    private RetourReason dummyReason;
+
 
     @BeforeEach
     public void setup() {
-        this.dummyRetourRequestDTO = new RetourRequestDTO();
-        this.dummyRetourRequestDTO.id = UUID.randomUUID();
+        dummyRetourRequestDTO = new RetourRequestDTO();
+        dummyRetourRequestDTO.id = UUID.randomUUID();
 
-        this.dummyProduct = new ProductSpecificationType();
-        this.dummyProduct.setId(UUID.randomUUID());
-        this.dummyRetouredProduct = new OrderItem();
-        this.dummyRetouredProduct.setProductType(dummyProduct);
-        this.dummyRetouredProducts = new HashSet<>();
-        this.dummyRetouredProducts.add(dummyRetouredProduct);
-        this.dummyRetourRequest = new RetourRequest();
-        this.dummyRetourRequest.setRetouredProducts(dummyRetouredProducts);
+        dummyProduct = new ProductSpecificationType();
+        dummyProduct.setId(UUID.randomUUID());
+
+        dummyRetouredProduct = new OrderItem();
+        dummyRetouredProduct.setProductType(dummyProduct);
+
+        dummyRetouredProducts = new HashSet<>();
+        dummyRetouredProducts.add(dummyRetouredProduct);
+
+        dummyRetourRequest = new RetourRequest();
+        dummyRetourRequest.setRetouredProducts(dummyRetouredProducts);
 
         when(retourRequestRepository.findById(any())).thenReturn(Optional.of(dummyRetourRequest));
-        doNothing().when(productDAO).incrementProductStock(any());
     }
 
     //  USER STORY #60
     //  Admin retours overzicht
 
-    // TEST Retour verzoek beoordelen (Task #70)
+    //  TEST Retour verzoek beoordelen (Task #70)
     @Test
     public void should_set_status_accepted_when_called() {
+        doNothing().when(productDAO).incrementProductStock(any());
         String expectedStatus = StaticDetails.RETOUR_ACCEPTED;
 
         RetourRequest actualRetourRequest = retourDAO.acceptRetourRequest(dummyRetourRequestDTO);
 
-        assertThat(actualRetourRequest, is(dummyRetourRequest));
-        assertThat(actualRetourRequest.getState(), is(expectedStatus));
-        verify(retourRequestRepository, times(1)).findById(any());
-        verify(retourRequestRepository, times(1)).save(dummyRetourRequest);
+        this.assert_retour_status(actualRetourRequest, expectedStatus);
         verify(productDAO, times(1)).incrementProductStock(dummyProduct.getId());
         verify(orderItemRepository, times(1)).saveAll(dummyRetouredProducts);
     }
 
     @Test void should_set_status_declined_when_called() {
+        String expectedStatus = StaticDetails.RETOUR_DECLINED;
 
+        RetourRequest actualRetourRequest = retourDAO.declineRetourRequest(dummyRetourRequestDTO);
+
+        this.assert_retour_status(actualRetourRequest, expectedStatus);
     }
 
-    // TEST retourverzoek zoeken op id (Task #68)
+    private void assert_retour_status(RetourRequest actualRetourRequest, String expectedStatus) {
+        assertThat(actualRetourRequest, is(dummyRetourRequest));
+        assertThat(actualRetourRequest.getState(), is(expectedStatus));
+        verify(retourRequestRepository, times(1)).findById(any());
+        verify(retourRequestRepository, times(1)).save(dummyRetourRequest);
+    }
+
+    //  TEST retourverzoek zoeken op id (Task #68)
     @Test
     public void should_return_request_when_given_valid_id() {
 
@@ -107,7 +110,7 @@ public class RetourDAOTests {
 
     }
 
-    // Test retourverzoeken terugvinden (Task #69)
+    //  Test retourverzoeken terugvinden (Task #69)
     @Test
     public void should_return_all_requests_when_called() {
 
@@ -116,7 +119,7 @@ public class RetourDAOTests {
     //  USER STORY #61
     //  Gebruiker orders overzicht
 
-    // Test: Retourverzoek indienen (Task #72)
+    //  Test: Retourverzoek indienen (Task #72)
     @Test
     public void should_create_request_when_all_inputs_valid() {
 
@@ -132,13 +135,13 @@ public class RetourDAOTests {
 
     }
 
-    // Test: Keuze uit 1 van de retourredenen (Task #71)
+    //  Test: Keuze uit 1 van de retourredenen (Task #71)
     @Test
     public void should_return_all_reasons() {
 
     }
 
-    // Test: 30 dagen retourbeleid (Task #73)
+    //  Test: 30 dagen retourbeleid (Task #73)
     @Test
     public void should_throw_exception_when_ordered_31_days_ago() {
 
