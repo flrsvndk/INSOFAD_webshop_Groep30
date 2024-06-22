ea23e77744012278de7f3f12be1d2f54c3bfa5fd
Bauke Brunschot <john.doe4@mail.com>
2024-06-22 13:33:20 +0200

    giftcard codes are now filled in on the cart page and update the price instantly

--- luxuryproducts-frontend/src/app/order/order.component.ts
+++ luxuryproducts-frontend/src/app/order/order.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import { CartService } from "../services/cart.service";
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
-import { Router } from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import { Order } from '../models/order.model';
 import {Adress} from "../models/adress.model";
 import {OrderItem} from "../models/order-item.model";
@@ -25,60 +25,72 @@ import {UserService} from "../services/user.service";
   styleUrls: ['./order.component.scss']
 })
 export class OrderComponent implements OnInit, OnDestroy {
-  public bestelForm: FormGroup;
-  public products_in_cart: ProductType[];
-  public order: Order;
-  public adress: Adress;
-  public totalPrice: number;
-  public orderItems: OrderItem[];
-  public promocode: Promocode;
+    public bestelForm: FormGroup;
+    public products_in_cart: ProductType[];
+    public order: Order;
+    public adress: Adress;
+    public totalPriceBeforePromocodeAndGiftcards: number;
+    public totalPriceWithPromocode: number;
+    public totalPrice: number;
+    public orderItems: OrderItem[];
+    public promocode: Promocode;
+    public giftcardString: string;
 
-  private addOrder$: Subscription;
+    private addOrder$: Subscription;
 
-  constructor(private giftcardService: GiftcardService,
-              private userService: UserService,
-              private cartService: CartService,
-              private router: Router,
-              private fb: FormBuilder,
-              private orderService: OrderService,) {
-      const navigation = this.router.getCurrentNavigation();
-      if (navigation?.extras.state) {
-          const state = navigation.extras.state as {
-              promocode: Promocode;
-          };
-          this.promocode = state.promocode;}
-  }
+    constructor(private giftcardService: GiftcardService,
+                private userService: UserService,
+                private cartService: CartService,
+                private router: Router,
+                private fb: FormBuilder,
+                private orderService: OrderService,
+                private route: ActivatedRoute) {
+        const navigation = this.router.getCurrentNavigation();
+        if (navigation?.extras.state) {
+            const state = navigation.extras.state as {
+                totalPriceBeforePromocodeAndGiftcards: number;
+                totalPriceWithPromocode: number;
+                totalPrice: number;
+                giftcardString: string;
+                promocode: Promocode;
+            };
+            this.totalPriceBeforePromocodeAndGiftcards = state.totalPriceBeforePromocodeAndGiftcards;
+            this.totalPriceWithPromocode = state.totalPriceWithPromocode;
+            this.totalPrice = state.totalPrice;
+            this.giftcardString = state.giftcardString;
+            this.promocode = state.promocode;
+        }
+    }
 
-  ngOnInit(): void {
-    this.products_in_cart = this.cartService.allProductsInCart();
-    // this.currentLoggedInUser = this.userService.getUserFromBearer(localStorage.getItem());
-    this.bestelForm = this.fb.group({
-      Postcode: [''],
-      Huisnummer: ['', [Validators.maxLength(5)]],
-      Opmerkingen: [''],
-      HouseNummerAddition: [''],
-      Opslaan: ['']
-    });
-    this.totalPrice = this.products_in_cart.reduce((total, product) => total + product.price * product.amount, 0);
-  }
+    ngOnInit(): void {
+        this.products_in_cart = this.cartService.allProductsInCart();
+        // this.currentLoggedInUser = this.userService.getUserFromBearer(localStorage.getItem());
+        this.bestelForm = this.fb.group({
+            Postcode: [''],
+            Huisnummer: ['', [Validators.maxLength(5)]],
+            Opmerkingen: [''],
+            HouseNummerAddition: [''],
+            Opslaan: ['']
+        });
+    }
 
-  ngOnDestroy() {
-    this.addOrder$?.unsubscribe();
-  }
+    ngOnDestroy() {
+        this.addOrder$?.unsubscribe();
+    }
 
-  public clearCart() {
-    this.cartService.clearCart();
-  }
+    public clearCart() {
+        this.cartService.clearCart();
+    }
 
-  public onSubmit() {
-      const formData = this.bestelForm.value;
+    public onSubmit() {
+        const formData = this.bestelForm.value;
 
-      this.orderItems = this.orderService.createOrderItems(this.products_in_cart);
+        this.orderItems = this.orderService.createOrderItems(this.products_in_cart);
 
-      this.order = {
-          notes: formData.Opmerkingen,
+        this.order = {
+            notes: formData.Opmerkingen,
 
-            adressDTO : this.adress = {
+            adressDTO: this.adress = {
                 zipcode: formData.Postcode,
                 houseNumber: formData.Huisnummer,
                 houseNumberAddition: formData.HouseNummerAddition,
@@ -93,54 +105,26 @@ export class OrderComponent implements OnInit, OnDestroy {
                 console.log('Order added successfully:', result);
                 this.clearCart();
                 this.router.navigateByUrl('/paymentsuccessful');
-                },
+            },
             (error) => {
                 console.error('Failed to add order:', error);
             }
-            );
+        );
         this.cartService.addOrder(this.order).subscribe(
-          (result) => {
-            console.log('Order added successfully:', result);
-            this.clearCart();
-            this.router.navigateByUrl('/paymentsuccessful');
-          },
-          (error) => {
-            console.error('Failed to add order:', error);
-          }
+            (result) => {
+                console.log('Order added successfully:', result);
+                this.clearCart();
+                this.router.navigateByUrl('/paymentsuccessful');
+            },
+            (error) => {
+                console.error('Failed to add order:', error);
+            }
         );
 
-        if (formData.Giftcards != "") {
-          this.useGiftcards(formData.Giftcards);
+        console.log(this.giftcardString);
+        if (this.giftcardString != null && this.giftcardString != "") {
+            // @ts-ignore
+            this.giftcardService.useGiftcards(this.giftcardString, this.totalPriceWithPromocode);
         }
-  }
-
-  public useGiftcards(giftcardsText: String) {
-    let unpaid = this.totalPrice;
-    let giftcardIds: String[] = giftcardsText.split(" ");
-    let giftcards: GiftcardOwned[] = this.giftcardService.ownedGiftcards;
-
-
-    for (let giftcard of giftcards) {
-      for (let id of giftcardIds) {
-        if (id == giftcard.id.toString()) {
-          console.log(giftcard);
-          let unpaidOld = unpaid;
-          unpaid -= Math.min(unpaid, giftcard.value);
-          giftcard.value -= Math.min(unpaidOld, giftcard.value);
-          this.giftcardService.lowerGiftcardValue(giftcard.value, giftcard.id).subscribe(
-              (result: HttpResponse) => {
-                console.log('Giftcard value updated successfully:', result);
-                this.router.navigateByUrl('/paymentsuccessful');
-              },
-              (error) => {
-                console.log(error);
-              }
-          );
-          break;
-        }
-      }
-      if (unpaid == 0) {
-        return;
-      }
     }
 }
