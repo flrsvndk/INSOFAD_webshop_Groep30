ecd43166f1cd1a342da8ff81cc677ad86f7e2418
Bauke Brunschot <john.doe4@mail.com>
2024-06-25 10:26:53 +0200

    makes the "Buy Giftcards" page only pop up in the header if you're logged in

--- luxuryproducts-frontend/cypress/e2e/giftcards.cy.ts
+++ luxuryproducts-frontend/cypress/e2e/giftcards.cy.ts
@@ -1,4 +1,4 @@
-describe('template spec', () => {
+describe('giftcards', () => {
 
   beforeEach(() => {
     cy.intercept('POST', '/api/auth/login', { fixture: 'login.json' }).as('login');
@@ -8,33 +8,49 @@ describe('template spec', () => {
     cy.get('input[id=email]').type('bob@bobsluxuryenterprise.com');
     cy.get('input[id=password1]').type('MegaGeheim6789!!');
     cy.get('button[id=login]').click();
+
+    cy.intercept('POST', '/api/giftcards', { fixture: 'example.json' }).as('login');
+    cy.intercept('GET', '/api/giftcards/getByOwner', {fixture: 'getByOwner.json'}).as('getByOwner');
+    cy.intercept('GET', '/api/giftcards/getByBuyer', {fixture: 'getByBuyer.json'}).as('getByBuyer');
+    cy.intercept('PUT', '/api/charge/', {fixture: 'example.json'}).as('charge');
+    cy.intercept('GET', '/api/giftcards/getAll', {fixture: 'getAll.json'}).as('getAll');
+    cy.intercept('DELETE', '/api/giftcards/**', {fixture: 'example.json'}).as('delete');
   });
 
-
-  it('passes', () => {
-    //testing giftcards
-    cy.intercept('POST', '/api/giftcards', { fixture: 'example.json' }).as('login');
+  //testing giftcards
+  it('Giftcard FAQ', () => {
     cy.visit("http://localhost:4200/giftcards");
     cy.contains("Frequently Asked Questions");
+  });
+
+  it('Giftcard kopen', () => {
+    cy.visit("http://localhost:4200/giftcards");
     cy.get(".btn").should('be.disabled')
     cy.get('[type="email"]').type('bob@bobsluxuryenterprise.com')
     cy.get(".btn").last().click()
+  });
 
-    // testing my-giftcards
-    cy.intercept('GET', '/api/giftcards/getByOwner', { fixture: 'getByOwner.json' }).as('getByOwner');
-    cy.intercept('GET', '/api/giftcards/getByBuyer', { fixture: 'getByBuyer.json' }).as('getByBuyer');
-    cy.intercept('PUT', '/api/charge/', { fixture: 'example.json' }).as('charge');
+  // testing my-giftcards
+  it('Gekochte giftcards inzien, gekregen giftcards inzien', () => {
     cy.visit("http://localhost:4200/my-giftcards");
     cy.contains("Bought");
     cy.contains("Sent");
-    cy.get("button").last().click();
+  });
 
-    // testing admin-giftcards
-    cy.intercept('GET', '/api/giftcards/getAll', { fixture: 'getAll.json' }).as('getAll');
-    cy.intercept('DELETE', '/api/giftcards/**', { fixture: 'example.json' }).as('delete');
+  it('Giftcard opladen', () => {
+    cy.visit("http://localhost:4200/my-giftcards");
+    cy.get("button").last().click();
+  });
+
+  // testing admin-giftcards
+  it('Alle giftcards inzien', () => {
     cy.visit("http://localhost:4200/admin/giftcards");
     cy.contains("Bought");
     cy.contains("Sent");
+  });
+
+  it('Giftcard annuleren', () => {
+    cy.visit("http://localhost:4200/admin/giftcards");
     cy.get("button").last().click();
-  })
+  });
 })
